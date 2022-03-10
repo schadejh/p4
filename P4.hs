c35f14c8769a3e0122c0bcec7cb66ed7bc113958
James H. Schader <schadejh@dukes.jmu.edu>
2022-03-10 16:47:13 -0500

    first

---
+++ P4.hs
@@ -0,0 +1,31 @@
+--
+-- CS 430 P4 (Haskell 2)
+--
+-- Name:
+--
+
+module P4 where
+import Defs
+
+-- calculate the result of an expression
+eval :: Expr -> Int
+eval e = -1
+
+-- count the total number of arithmetic operations in an expression
+countOps :: Expr -> Int
+countOps e = -1
+
+-- calculate the height of the expression tree
+-- height of one node = 1
+height :: Expr -> Int
+height e = -1
+
+-- flatten the expression into a postfix string representation
+-- use "(show i)" to convert int i to a string
+postfix :: Expr -> String
+postfix e = ""
+
+-- extract a sorted list of all unique integers in an expression
+uniqInts :: Expr -> [Int]
+uniqInts e = []
+
